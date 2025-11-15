b99d772edf86f42276c06786cced154d537d6cbf
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-11-15 10:38:07 +0100

    initial import sbatch files

---
+++ preoperativeSAM/utils/sbatch_file/sam_train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=21:00:00              # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=sam.err              # standard error file
+#SBATCH --output=sam.out             # standard output file
+#SBATCH --account=IscrC_AIM-ORAL     # account name
+
+python -m --conf preoperativeSAM.tools.train\
+          --task PrePostiUS\
+          --model_name SAM\
+          --dataset_loader pre_and_post\
+          --keep_log
